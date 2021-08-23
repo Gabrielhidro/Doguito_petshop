55ca9edf37bf1b1b67085c2ed7d823777f7e9abc
gabrielhidro <gabrielhidro.dev@gmail.com>
2021-08-23 17:08:49 -0300

    Add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import './assets/css/base/base.css'
+import Home from './paginas/Home'
+import Sobre from './paginas/Sobre'
 
 function App() {
+
+  function Router(){
+    const location = window.location.pathname
+    if(location === '/sobre'){
+      return <Sobre />
+    }
+    else {
+      return <Home />
+    }
+  }
+
   return (
   <>
-
+    { Router() }
   </>
   )
 }
