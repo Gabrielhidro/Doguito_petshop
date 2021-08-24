7c119d3f6ad5747f64d58518cc81a705f01ba5ba
gabrielhidro <gabrielhidro.dev@gmail.com>
2021-08-24 15:22:26 -0300

    Configure error page for post

--- src/paginas/Post.jsx
+++ src/paginas/Post.jsx
@@ -1,14 +1,19 @@
 import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
+import { useHistory, useParams } from "react-router-dom"
 import { busca } from "../api/api"
 import '../assets/css/post.css'
 
 export default function Post(){
+
+    let history = useHistory()
     const {id} = useParams()
     const [ post, setPost ] = useState({})
     
     useEffect(() => {
         busca(`/posts/${id}`, setPost)
+        .catch(() => {
+            history.push('/404')
+        })
     }, [id])
 
     console.log(post);
