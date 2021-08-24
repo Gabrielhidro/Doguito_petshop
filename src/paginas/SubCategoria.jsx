0efa0a7cccc67e95849c24c4d67a088f97b35722
gabrielhidro <gabrielhidro.dev@gmail.com>
2021-08-24 17:45:01 -0300

    Create subcategories

---
+++ src/paginas/SubCategoria.jsx
@@ -0,0 +1,11 @@
+import { useParams } from "react-router-dom";
+import ListaPost from "../components/ListaPost";
+
+export default function SubCategoria(){
+
+const { subcategoria } = useParams()
+
+    return (
+        <ListaPost url={`/posts?subcategoria=${subcategoria}`}/>
+    )
+}
