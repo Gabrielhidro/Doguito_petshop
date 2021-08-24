ad4c667add1955a7a2a2ba38c3bea2cd77ac8bab
gabrielhidro <gabrielhidro.dev@gmail.com>
2021-08-24 17:57:39 -0300

    Configure subcategories routes

--- src/paginas/Categoria.jsx
+++ src/paginas/Categoria.jsx
@@ -1,32 +1,52 @@
-import React, { useEffect, useState } from 'react'
-import { Route, useParams, useRouteMatch } from 'react-router-dom'
-import { busca } from '../api/api'
-import '../assets/css/blog.css'
-import ListaCategorias from '../components/ListaCategorias'
-import ListaPost from '../components/ListaPost'
+import React, { useEffect, useState } from "react";
+import { Link, Route, Switch, useParams, useRouteMatch } from "react-router-dom";
+import { busca } from "../api/api";
+import "../assets/css/blog.css";
+import ListaCategorias from "../components/ListaCategorias";
+import ListaPost from "../components/ListaPost";
+import SubCategoria from "./SubCategoria";
 
-export default function Categoria(){
+export default function Categoria() {
+  const { id } = useParams();
+  const { url, path } = useRouteMatch();
+  const [subcategorias, setSubcategorias] = useState([]);
 
-    const {id} = useParams()
-    const { path } = useRouteMatch()
-    const [ subcategorias, setSubcategorias ] = useState([]) 
+  useEffect(() => {
+    busca(`/categorias/${id}`, (categoria) => {
+      setSubcategorias(categoria.subcategorias);
+    });
+  }, [id]);
 
-    useEffect(() => {
-        busca(`/categorias/${id}`, (categoria)=> {
-            setSubcategorias(categoria.subcategorias)
-        })
-    }, [id])
+  return (
+    <>
+      <div className="container">
+        <h2 className="titulo-pagina">Pet Notícias</h2>
+      </div>
 
-    return(
-        <>
-        <div className="container">
-            <h2 className="titulo-pagina">Pet Notícias</h2>
-        </div>
+      <ListaCategorias />
 
-        <ListaCategorias />
+      <ul className="lista-categorias container flex">
+        { subcategorias.map((subcategoria) => {
+            return (
+                <li
+                    className={`lista-categorias__categoria lista-categorias__categoria--${id}`}
+                    key={subcategoria}
+                >
+                    <Link to={`${url}/${subcategoria}`}>
+                        {subcategoria}
+                    </Link>
+                </li>
+            )
+        })}
+      </ul>
+      <Switch>
         <Route exact path={`${path}/`}>
-            <ListaPost url={`/posts?categoria=${id}`}/>
+          <ListaPost url={`/posts?categoria=${id}`} />
         </Route>
-        </>
-    )
+        <Route path={`${path}/:subcategoria`}>
+          <SubCategoria />
+        </Route>
+      </Switch>
+    </>
+  );
 }
