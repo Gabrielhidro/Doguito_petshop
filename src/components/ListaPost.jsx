aa75b18f133229d7410f3cee2ae68aa4f91bb240
gabrielhidro <gabrielhidro.dev@gmail.com>
2021-08-24 14:55:14 -0300

    Gete api datas and render in html

--- src/components/ListaPost.jsx
+++ src/components/ListaPost.jsx
@@ -1,9 +1,30 @@
-import { busca } from '../api/api'
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { busca } from "../api/api";
 
-export default function ListaPost (){
-    return (
-        <section className="post container">
+export default function ListaPost( { url } ) {
+  const [posts, setPosts] = useState([]);
 
-        </section>
-    )
+  useEffect(() => {
+    busca(url, setPosts)
+  }, [])
+
+  return (
+    <section className="posts container">
+      {posts.map((post) => {
+        return (
+          <Link className={`cartao-post cartao-post--${post.categoria}`}>
+            <article key={post.id}>
+                <h3 className="cartao-post__titulo">
+                    {post.title}
+                    <p className="cartao-post__meta">
+                        {post.metadescription}
+                    </p>
+                </h3>
+            </article>
+          </Link>
+        );
+      })}
+    </section>
+  );
 }
