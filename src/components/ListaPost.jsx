e437cc9aa5e1fb36b82eec1d6ba7f2d567266035
gabrielhidro <gabrielhidro.dev@gmail.com>
2021-08-24 15:20:06 -0300

    Render single post

--- src/components/ListaPost.jsx
+++ src/components/ListaPost.jsx
@@ -2,25 +2,24 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { busca } from "../api/api";
 
-export default function ListaPost( { url } ) {
+export default function ListaPost({ url }) {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    busca(url, setPosts)
-  }, [])
+    busca(url, setPosts);
+  }, []);
+
 
   return (
     <section className="posts container">
       {posts.map((post) => {
         return (
-          <Link className={`cartao-post cartao-post--${post.categoria}`}>
+          <Link to={`/posts/${post.id}`} className={`cartao-post cartao-post--${post.categoria}`}>
             <article key={post.id}>
-                <h3 className="cartao-post__titulo">
-                    {post.title}
-                    <p className="cartao-post__meta">
-                        {post.metadescription}
-                    </p>
-                </h3>
+              <h3 className="cartao-post__titulo">
+                {post.title}
+                <p className="cartao-post__meta">{post.metadescription}</p>
+              </h3>
             </article>
           </Link>
         );
